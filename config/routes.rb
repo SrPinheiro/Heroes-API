f0235baa216339f56b279f601af55064d68c6f60
SrPinheiro <sr.pinheiro.ifnmg@gmail.com>
2022-12-28 19:38:00 -0300

    First heroes add

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :heroes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'ping' => ->(_) { [200, {}, ['pong']] }
+
+  #root to: -> (_){[200, {}, ['e que hoje vai ter festinha']]}
 end
